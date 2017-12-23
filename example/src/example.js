45b73c560f02f3f0e8f3654c630d3f938bbadf21
Rick Hopkins <rick.hopkins@gmail.com>
2017-12-23 09:39:22 -0500

    Added some more example code to show various ways of binding and injecting.

--- example/src/example.js
+++ example/src/example.js
@@ -21,38 +21,64 @@ export class DepTwo {
 	}
 }
 
+export class DepThree {
+	constructor(depFour) {
+		if (depFour instanceof DepFour) {
+			console.log('I am an instance of DepFour');
+		}
+		console.log(depFour);
+		this.msg = '';
+	}
+}
+
+export class DepFour {
+	constructor() {
+		console.log('I am a dependency of DepThree also bound at startup.');
+		this.loaded = true;
+	}
+}
+
+Injector.bind('depThree', DepThree).addDependency('depFour');
+Injector.bind('depFour', DepFour);
+
 class Test1 {
-	constructor(@Inject('depOne') depOne, @Inject('depTwo') depTwo) {
+	constructor(@Inject('depOne') depOne, @Inject('depTwo') depTwo, @Inject('depThree') depThree) {
 		depOne.msg = 'Singleton: Test1';
 		depTwo.msg = 'Non-Singleton: Test1';
+		depThree.msg = 'DepThree was bound at startup as a singleton. Test 1'
 	}
 }
 
 class Test2 {
-	constructor(@Inject('depOne') depOne, @Inject('depTwo') depTwo) {
+	constructor(@Inject('depOne') depOne, @Inject('depTwo') depTwo, @Inject('depThree') depThree) {
 		depOne.msg += ': Test2';
 		depTwo.msg += ': Test2';
+		depThree.msg += ': Test2';
 	}
 }
 
 class TestA {
-	constructor(depOne, depTwo) {
+	constructor(depOne, depTwo, depThree) {
 		depOne.msg += ': TestA';
 		depTwo.msg += ': TestA';
+		depThree.msg += ': TestA';
 	}
 }
 
 class TestB {
-	constructor(depOne, depTwo) {
+	constructor(depOne, depTwo, depThree) {
 		depOne.msg += ': TestB';
 		depTwo.msg += ': TestB';
+		depThree.msg += ': TestB';
+
 
 		console.log(depOne.msg);
 		console.log(depTwo.msg);
+		console.log(depThree.msg);
 	}
 }
 
 let test1 = new Test1();
 let test2 = new Test2();
-let testa = Injector.construct(TestA, ['depOne', 'depTwo']);
-let testb = Injector.construct(TestB, ['depOne', 'depTwo']);
+let testa = Injector.construct(TestA, ['depOne', 'depTwo', 'depThree']);
+let testb = Injector.construct(TestB, ['depOne', 'depTwo', 'depThree']);
